2613f1c67598f322e342a743c82d5f4cf9195cdf
Ricky Huang <tingbin0330@gmail.com>
2022-08-31 00:25:38 +0800

    create extract function

--- src/Generic/Hydrator/DataTransferObject.php
+++ src/Generic/Hydrator/DataTransferObject.php
@@ -24,4 +24,26 @@ class DataTransferObject
         }
         return $object;
     }
+
+    /**
+     * @param $object
+     * @return array
+     */
+    public static function extract($object): array
+    {
+        $array = array();
+        $class = get_class($object);
+        $methodList = get_class_methods($class);
+        foreach ($methodList as $method) {
+            preg_match('/^(get)(.*?)$/i', $method, $matches);
+            $prefix = $matches[1] ?? '';
+            $key = $matches[2] ?? '';
+            $key = strtolower(substr($key, 0, 1)) . substr($key, 1);
+            if ($prefix == 'get') {
+                $array[$key] = $object->$method();
+            }
+        }
+        return $array;
+    }
+}
 }
