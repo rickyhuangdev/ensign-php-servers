ee4df99ae02c90db2e42650df1c9dfef5ffae13c
Ricky Huang <tingbin0330@gmail.com>
2022-09-10 01:46:26 +0800

    update the json exception response

--- src/Exceptions/JsonResponseException.php
+++ src/Exceptions/JsonResponseException.php
@@ -9,18 +9,21 @@ use Throwable;
 
 class JsonResponseException extends BaseException
 {
-    public function handle(Throwable $throwable, ResponseInterface $response): ResponseInterface
+    public function handle(Throwable $throwable, ResponseInterface $response)
     {
         $responseContents = $response->getBody()->getContents();
         $responseContents = json_decode($responseContents, true);
-        var_dump($responseContents);
-        var_dump($throwable->getLine(), $throwable->getMessage(), $throwable->getTraceAsString(), $throwable->getCode(), $throwable->getPrevious());
-        if ($throwable instanceof ModelNotFoundException) {
-            $responseContents['error']['errorMessage'] = 'Sorry, the request data cannot be found';
-            $responseContents['error']['errorCode'] = $responseContents['error']['code'] = 404;
-        } else {
-            $responseContents['error']['errorMessage'] = $responseContents['error']['message'] ?? $throwable->getMessage();
-            $responseContents['error']['errorCode'] = $code ?? 500;
+        if (!empty($responseContents['error'])) {
+            $port = null;
+            $config = ApplicationContext::getContainer()->get(ConfigInterface::class);
+            $servers = $config->get('server.servers');
+            foreach ($servers as $k => $server) {
+                if ($server['name'] == 'jsonrpc-http') {
+                    $port = $server['port'];
+                    break;
+                }
+            }
+            $responseContents['error']['message'] .= " - {$this->appName}:{$port}";
         }
         $data = json_encode($responseContents, JSON_UNESCAPED_UNICODE);
         return $response->withStatus(200)->withBody(new SwooleStream($data));
