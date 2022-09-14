db443b02a624022acd9fa927aae26aca246dc3c7
Ricky Huang <tingbin0330@gmail.com>
2022-09-14 10:56:11 +0800

    update use exception

--- src/Exceptions/ModelNotDefined.php
+++ src/Exceptions/ModelNotDefined.php
@@ -1,10 +1,12 @@
 <?php
 declare(strict_types=1);
+
 namespace Rickytech\Library\Exceptions;
 
+use Hyperf\Server\Exception\ServerException;
 use Throwable;
 
-class ModelNotDefined extends \RuntimeException
+class ModelNotDefined extends ServerException
 {
     public function __construct(string $message = "", int $code = 0, ?Throwable $previous = null)
     {
