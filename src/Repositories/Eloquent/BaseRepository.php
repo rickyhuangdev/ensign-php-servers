ae0aa837cfa578fd3e8464baa671e553d3c4c1c7
Ricky Huang <tingbin0330@gmail.com>
2022-09-14 21:21:17 +0800

    update the redis cache function

--- src/Repositories/Eloquent/BaseRepository.php
+++ src/Repositories/Eloquent/BaseRepository.php
@@ -15,7 +15,7 @@ abstract class BaseRepository
     public function __construct()
     {
         $this->model = $this->getModelClass();
-//        $this->cache = PhpRedis::getPhpRedis();
+        $this->cache = PhpRedis::getPhpRedis();;
     }
 
     protected function getModelClass()
