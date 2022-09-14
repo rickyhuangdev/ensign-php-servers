8005b37d24d26152530d154cf2dc086150fc5055
Ricky Huang <tingbin0330@gmail.com>
2022-09-15 00:22:41 +0800

    update the tree list

--- src/Traits/TreeList.php
+++ src/Traits/TreeList.php
@@ -7,7 +7,7 @@ use Hyperf\Utils\Collection;
 
 trait TreeList
 {
-    public function toTreeList(array|Collection|LengthAwarePaginator $source, $id = 0, $level = 0, string $primaryKey = 'id', string $parentKey = 'pid', string $childrenKey = 'children'): array
+    public function toTreeList(array|Collection|LengthAwarePaginator $source, $id = 0, $level = 0, string $primaryKey = 'id', string $parentKey = 'pid', string $childrenKey = 'children', string $labelName, string $value): array
     {
         if ($source instanceof LengthAwarePaginator) {
             $source = $source->getCollection()->toArray();
@@ -15,13 +15,27 @@ trait TreeList
             $source = $source->toArray();
         }
         $list = array();
-        foreach ($source as $k => $v) {
-            if ($v[$parentKey] == $id) {
-                $v['level'] = $level;
-                $v[$childrenKey] = $this->toTreeList($source, $v[$primaryKey], $level + 1);
-                $list[] = $v;
+        if ($labelName) {
+            foreach ($source as $k => $v) {
+                if ($v[$parentKey] == $id) {
+                    $v['level'] = $level;
+                    $v['label'] = $labelName;
+                    $v['value'] = $value;
+                    $v[$childrenKey] = $this->toTreeList($source, $v[$primaryKey], $level + 1);
+                    $list[] = $v;
+                }
+            }
+        } else {
+            foreach ($source as $k => $v) {
+                if ($v[$parentKey] == $id) {
+                    $v['level'] = $level;
+                    $v[$childrenKey] = $this->toTreeList($source, $v[$primaryKey], $level + 1);
+                    $list[] = $v;
+                }
             }
         }
+
         return $list;
     }
+
 }
