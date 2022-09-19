80d57d0b0d45230035d30ce25218aa697d2ce5ff
Ricky Huang <tingbin0330@gmail.com>
2022-09-19 16:34:10 +0800

    create Unauthorization exception

--- src/Exceptions/Unauthorization.php
+++ src/Exceptions/Unauthorization.php
@@ -6,7 +6,7 @@ use Throwable;
 
 class Unauthorization extends \Exception
 {
-    public function __construct(string $message = "", int $code = 401, ?Throwable $previous = null)
+    public function __construct(string $message = "Unauthorized", int $code = 401, ?Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
