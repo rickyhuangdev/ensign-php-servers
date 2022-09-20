95bd1a2ad3094609ffd3f349f194a28619bf7a82
Ricky Huang <tingbin0330@gmail.com>
2022-09-20 16:38:16 +0800

    update AuthorizationException status code

--- src/Exceptions/AuthorizationException.php
+++ src/Exceptions/AuthorizationException.php
@@ -6,7 +6,7 @@ use Throwable;
 
 class AuthorizationException extends \Exception
 {
-    public function __construct(string $message = "Unauthorized", int $code = 401, ?Throwable $previous = null)
+    public function __construct(string $message = "Unauthorized", int $code = 403, ?Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
