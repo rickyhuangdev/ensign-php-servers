51f9664d104eac9c5aed7d9513a139ae49767e37
Ricky Huang <tingbin0330@gmail.com>
2022-09-21 10:31:57 +0800

    update the cache function

--- src/Services/Cache/Instances/PhpRedis.php
+++ src/Services/Cache/Instances/PhpRedis.php
@@ -91,7 +91,7 @@ final class PhpRedis implements Cache
      */
     public function get($key, $default = null)
     {
-        return $this->redisClient->get($key);
+        return json_decode($this->redisClient->get($key),true);
     }
 
     /**
@@ -128,7 +128,7 @@ final class PhpRedis implements Cache
      */
     public function put($key, $value, int $ttl = 3600): bool
     {
-        return $this->redisClient->set($key, $value, $ttl);
+        return $this->redisClient->set($key, json_encode($value, JSON_UNESCAPED_UNICODE), $ttl);
     }
 
     /**
