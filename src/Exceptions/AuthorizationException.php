aaadc0de9a3e477088294fdefc2a6c4843b9006c
Ricky Huang <tingbin0330@gmail.com>
2022-10-26 12:26:26 +0800

    update result response

--- src/Exceptions/AuthorizationException.php
+++ src/Exceptions/AuthorizationException.php
@@ -2,11 +2,12 @@
 
 namespace Rickytech\Library\Exceptions;
 
+use Rickytech\Library\Response\Result;
 use Throwable;
 
 class AuthorizationException extends \Exception
 {
-    public function __construct(string $message = "Unauthorized", int $code = 403, ?Throwable $previous = null)
+    public function __construct(string $message = "Unauthorized", int $code = Result::SESSION_EXPIRED, ?Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
