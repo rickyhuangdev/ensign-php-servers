84cf9015690a0a56215408ad3bca6de5225bfa45
Ricky Huang <tingbin0330@gmail.com>
2022-10-26 11:43:32 +0800

    update result response

--- src/Validator/BaseValidator.php
+++ src/Validator/BaseValidator.php
@@ -5,6 +5,7 @@ namespace Rickytech\Library\Validator;
 use Hyperf\Di\Annotation\Inject;
 use Hyperf\Validation\Contract\ValidatorFactoryInterface;
 use Rickytech\Library\Exceptions\FormRequestException;
+use Rickytech\Library\Response\Result;
 
 
 class BaseValidator
@@ -16,6 +17,6 @@ class BaseValidator
 
     protected function formRequestError()
     {
-        throw new FormRequestException($this->validator->errors()->first(), 422);
+        throw new FormRequestException($this->validator->errors()->first(), Result::VALIDATE_FAILED);
     }
 }
