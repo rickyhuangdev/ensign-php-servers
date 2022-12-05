0bd2f7eac62b260782fc3779cab536aadcc328c0
Ricky <ricky_huang_hkg@ensignfreight.com>
2022-12-05 09:16:27 +0800

    update the base service

--- src/Services/Database/Services/BaseServices.php
+++ src/Services/Database/Services/BaseServices.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 namespace Rickytech\Library\Services\Database\Services;
 
 use Hyperf\DbConnection\Db;
+use Hyperf\Redis\Redis;
+use Hyperf\Utils\ApplicationContext;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 
 abstract class BaseServices
 {
@@ -11,6 +15,17 @@ abstract class BaseServices
      * @var Object $dao
      */
     protected object $dao;
+
+    protected ?Redis $cache = null;
+
+    public function __construct()
+    {
+        try {
+            $this->cache = ApplicationContext::getContainer()->get(\Hyperf\Redis\Redis::class);
+        } catch (NotFoundExceptionInterface|ContainerExceptionInterface $e) {
+            throw new \RuntimeException($e->getMessage(), 500);
+        }
+    }
 
     public function transaction(callable $closure, bool $isTran = true)
     {
