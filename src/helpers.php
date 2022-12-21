673560c1ae49de2700acfbadc9fe7898c0e69fef
Ricky <ricky_huang_hkg@ensignfreight.com>
2022-12-21 10:37:48 +0800

    create is json function

--- src/helpers.php
+++ src/helpers.php
@@ -5,7 +5,7 @@ use Hyperf\Paginator\LengthAwarePaginator;
 use Hyperf\Utils\Collection;
 
 if (!function_exists('convertArrayToTree')) {
-    function convertArrayToTree(array|Collection|LengthAwarePaginator $data, $id = 0, $level = 0, string $primaryKey = 'id', string $parentKey = 'pid', string $childrenKey = 'children')
+    function convertArrayToTree(array|Collection|LengthAwarePaginator $data, $id = 0, $level = 0, string $primaryKey = 'id', string $parentKey = 'pid', string $childrenKey = 'children'): array
     {
         if ($data instanceof Collection) {
             $data = $data->toArray();
@@ -15,9 +15,9 @@ if (!function_exists('convertArrayToTree')) {
 
         $list = array();
         foreach ($data as $k => $v) {
-            if ($v[$parentKey] == $id) {
+            if ($v[$parentKey] === $id) {
                 $v['level'] = $level;
-                $v[$childrenKey] = convertArrayToTree($data, $v["{$primaryKey}"], $level + 1);
+                $v[$childrenKey] = convertArrayToTree($data, $v[(string)($primaryKey)], $level + 1);
                 $list[] = $v;
             }
         }
@@ -38,6 +38,15 @@ if (!function_exists('dashesToCamelCase')) {
 if (!function_exists('camelCaseToUnderscore')) {
     function camelCaseToUnderscore($input): string
     {
-        return ltrim(strtolower(preg_replace('/[A-Z]([A-Z](?![a-z]))*/', '_$0', $input)), '_');
+        return strtolower(ltrim(preg_replace('/[A-Z]([A-Z](?![a-z]))*/', '_$0', $input), '_'));
+    }
+}
+if (!function_exists('isJSON')) {
+    /**
+     * @throws JsonException
+     */
+    function isJSON($string): bool
+    {
+        return is_string($string) && is_array(json_decode($string, true, 512, JSON_THROW_ON_ERROR)) && (json_last_error() === JSON_ERROR_NONE);
     }
 }
