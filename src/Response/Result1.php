a4117ec42bd67e629b9ab8bdf75a58a2bf295dfe
Ricky <ricky_huang_hkg@ensignfreight.com>
2023-01-09 10:03:20 +0800

    create the response result

--- src/Response/Result1.php
+++ src/Response/Result1.php
@@ -3,9 +3,6 @@ declare(strict_types=1);
 
 namespace Rickytech\Library\Response;
 
-use Hyperf\Paginator\LengthAwarePaginator;
-use Hyperf\Paginator\Paginator;
-use Hyperf\Utils\Collection as UtilCollection;
 use Hyperf\HttpServer\Contract\ResponseInterface;
 use Psr\Container\ContainerInterface;
 
@@ -39,7 +36,11 @@ class Result1
 
     public function success(mixed $data = null, string|null $message = '', string|int $code = self::SUCCESS)
     {
-        return $this->result(true, $data, $message, $code);
+        return $this->response->json([
+            'success' => true,
+            'code' => $code,
+            'data' => $data,
+        ]);
     }
 
     public function fail(int $code, string $message = '')
@@ -73,22 +74,22 @@ class Result1
             'data' => $data,
             'message' => $message
         ];
-        $responseData = null;
-        if ($data instanceof Collection || $data instanceof Model || $data instanceof UtilCollection) {
-            $responseData = $data->toArray();
-        }
-        if ($data instanceof LengthAwarePaginator || $data instanceof ResourceCollection || $data instanceof Paginator) {
-            $responseData = [
-                'items' => $data->getCollection() ?? $data->items(),
-                'total' => $data->total() ?? $data->count(),
-                'current' => $data->currentPage(),
-                'pageSize' => $data->perPage(),
-                'totalPage' => $data->lastPage() ?? 0,
-            ];
-        }
-        if ($data) {
-            return $this->response->json([...$response, 'data' => $responseData]);
-        }
+//        $responseData = null;
+//        if ($data instanceof Collection || $data instanceof Model || $data instanceof UtilCollection) {
+//            $responseData = $data->toArray();
+//        }
+//        if ($data instanceof LengthAwarePaginator || $data instanceof ResourceCollection || $data instanceof Paginator) {
+//            $responseData = [
+//                'items' => $data->getCollection() ?? $data->items(),
+//                'total' => $data->total() ?? $data->count(),
+//                'current' => $data->currentPage(),
+//                'pageSize' => $data->perPage(),
+//                'totalPage' => $data->lastPage() ?? 0,
+//            ];
+//        }
+//        if ($data) {
+//            return $this->response->json([...$response, 'data' => $responseData]);
+//        }
         return $this->response->json($response);
     }
 }
