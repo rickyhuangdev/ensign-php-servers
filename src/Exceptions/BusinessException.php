526fb5c27fb90b5a2b0795c65a491882744ac2bf
Ricky <ricky_huang_hkg@ensignfreight.com>
2023-01-09 10:31:01 +0800

    create the response result

--- src/Exceptions/BusinessException.php
+++ src/Exceptions/BusinessException.php
@@ -11,8 +11,12 @@ declare(strict_types=1);
 namespace Rickytech\Library\Exceptions;
 
 use Hyperf\Server\Exception\ServerException;
+use Throwable;
 
 class BusinessException extends ServerException
 {
-
+    public function __construct(string $message = "", int $code = 500, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
