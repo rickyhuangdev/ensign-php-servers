b4cce385c4a32e7c9fcbbf17fe9165e771f2c838
Ricky <ricky_huang_hkg@ensignfreight.com>
2023-01-14 14:36:31 +0800

    update the response

--- src/Response/Result1.php
+++ src/Response/Result1.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Rickytech\Library\Response;
@@ -23,44 +24,35 @@ class Result1
 
     public function success(mixed $data = null, string|null $message = '', string|int $code = self::SUCCESS)
     {
+        if (isset($data['data'], $data['current_page'], $data['per_page'])) {
+            return $this->pageResult($data);
+        }
         return $this->response->json([
             'success' => true,
-            'code' => $code,
-            'data' => $data,
+            'code'    => $code,
+            'data'    => $data,
         ]);
     }
 
     public function fail(int $code, string $message = '')
     {
         return $this->response->json([
-            'success' => false,
+            'success'   => false,
             'errorCode' => $code,
-            'errorMsg' => $message,
+            'errorMsg'  => $message,
         ]);
     }
 
     public function pageResult(mixed $data)
     {
-//        if ($data instanceof Collection || $data instanceof Model || $data instanceof UtilCollection) {
-//            $data = $data->toArray();
-//        }
-//        if ($data instanceof LengthAwarePaginator || $data instanceof ResourceCollection || $data instanceof Paginator) {
-//            $data = [
-//                'items' => $data->getCollection() ?? $data->items(),
-//                'total' => $data->total() ?? $data->count(),
-//                'current' => $data->currentPage(),
-//                'pageSize' => $data->perPage(),
-//                'totalPage' => $data->lastPage() ?? 0,
-//            ];
-//        }
         return $this->response->json([
             'success' => true,
-            'code' => 200,
-            'data' => [
-                'items' => $data['data'],
-                'total' => $data['total'],
-                'current' => $data['current_page'],
-                'pageSize' => $data['per_page'],
+            'code'    => 200,
+            'data'    => [
+                'items'     => $data['data'],
+                'total'     => $data['total'],
+                'current'   => $data['current_page'],
+                'pageSize'  => $data['per_page'],
                 'totalPage' => $data['last_page'],
             ],
         ]);
